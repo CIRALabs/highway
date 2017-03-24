66ee947d28272261f006df5fdea08657c7bc2ca7
Michael Richardson <mcr@sandelman.ca>
2017-03-24 11:18:17 -0500

    initial test case for generating a simple voucher

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,3 +1,17 @@
 class Voucher < ActiveRecord::Base
   belongs_to :device
+
+  def jsonhash(today = DateTime.utc.now)
+    h2 = Hash.new
+    h2["nonce"]      = nonce
+    h2["created-on"] = created_at
+    h2["device-identifier"] = device.eui64
+    h2["assertion"]         = "logged"
+    h2["owner"]             = "foo" # self.owner.der_spki.to_base64
+
+    # return it all.
+    h1 = Hash.new
+    h1["ietf-voucher:voucher"] = h2
+    h1
+  end
 end
--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -3,11 +3,32 @@ require 'rails_helper'
 RSpec.describe Voucher, type: :model do
   fixtures :all
 
+  def generate_random_nonce
+    "hello1"
+  end
+
   describe "relations" do
     it "should refer to a device" do
       v1 = vouchers(:almec_v1)
 
       expect(v1.device).to eq(devices(:almec))
+    end
+  end
+
+  describe "json creation" do
+    it "should create json representation in ietf-anima-voucher format" do
+      v1 = vouchers(:almec_v1)
+
+      today  = '2017-01-01'.to_date
+
+      json = v1.jsonhash(today)
+      expect(json["ietf-voucher:voucher"].class).to be Hash
+      json1 = json["ietf-voucher:voucher"]
+      expect(json1["nonce"]).to                     eq(v1.nonce)
+      expect(json1["created-on"].to_datetime).to    eq(today)
+      expect(json1["device-identifier"]).to         eq(v1.device.eui64)
+      expect(json1["assertion"]).to                  eq("logged")
+      expect(json1["owner"]).to_not be_nil
     end
   end
 
