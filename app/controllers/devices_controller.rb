25d98cb47a09bf401e6a58ad8462c45b005c4792
Michael Richardson <mcr@sandelman.ca>
2017-03-24 09:44:05 -0400

    added security to devices controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,4 +1,6 @@
 class DevicesController < ApplicationController
+  before_filter :require_admin
+
   active_scaffold :device do |config|
     #config.columns = [ :eui64, :customer, :hostname ]
   end
