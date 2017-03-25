1decda027507fd6c4553b8ab02d04237e74c9655
Michael Richardson <mcr@sandelman.ca>
2017-03-25 14:03:15 -0500

    added owner and owner certificate

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,7 @@
 class Owner < ActiveRecord::Base
   has_many :vouchers
+
+  def certder
+    @cert ||= OpenSSL::X509::Certificate.new(self.certificate)
+  end
 end
--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Owner, type: :model do
+  fixtures :all
+
   describe "relations" do
     it { should have_many(:vouchers) }
+  end
+
+  describe "certificates" do
+    it "should have a certificate" do
+      o1 = owners(:owner1)
+      expect(o1.certificate).to_not be_nil
+
+      expect(o1.certder).to_not be_nil
+      expect(o1.certder.subject.to_s).to eq("/C=CA/ST=Ontario/L=Ottawa/O=Owner Example One/OU=Not Very/CN=owner1.example.com/emailAddress=owner1@example.com")
+    end
   end
 end
