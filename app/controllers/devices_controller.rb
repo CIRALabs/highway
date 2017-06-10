4adcca882f6d18018b96d58dda42bfc1d8a2db81
Michael Richardson <mcr@sandelman.ca>
2017-06-10 12:49:45 -0400

    first test cases with SSL access to EST API

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,5 +1,5 @@
 class DevicesController < ApplicationController
-  before_filter :require_admin
+  before_action :require_admin
 
   active_scaffold :device do |config|
     #config.columns = [ :eui64, :customer, :hostname ]
--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,5 +1,5 @@
 class VouchersController < ApplicationController
-  before_filter :require_admin
+  before_action :require_admin
 
   active_scaffold :voucher do |config|
     #config.columns = [ :hostid, :customer, :hostname ]
