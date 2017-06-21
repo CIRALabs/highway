a986720043d1096bed5a46ee59bd14de3ffcc1e6
Michael Richardson <mcr@sandelman.ca>
2017-06-21 12:10:58 -0400

    initial work to parse JWT from disk

--- spec/models/voucher_request_spec.rb
+++ spec/models/voucher_request_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe VoucherRequest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "relations" do
+    it { should belong_to(:voucher) }
+    it { should belong_to(:owner) }
+    it { should belong_to(:device) }
+  end
+
+  describe "voucher signed" do
+    it "should validate a signed voucher request" do
+      v1 = voucher_requests(:sample_request1)
+
+    end
+  end
+
+  describe "voucher input request" do
+    it "should read a voucher request from disk" do
+      token = nil
+      File.open("spec/files/jada_abcd.jwt","r") do |f|
+        token = f.read
+
+        decoded_token = JWT.decode token, nil, false
+        byebug
+
+        part1js = JSON.parse(Base64.urlsafe_decode64(part1))
+        part2js = JSON.parse(Base64.urlsafe_decode64(part2))
+        part3bin = Base64.urlsafe_decode64(part3)
+      end
+    end
+  end
+
 end
