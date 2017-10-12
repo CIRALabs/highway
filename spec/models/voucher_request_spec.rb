a96755033235b4f0ec0e121f00650339a1c23177
Michael Richardson <mcr@sandelman.ca>
2017-10-12 16:26:35 -0400

    update test case with correctly parboiled voucher request from JRC

--- spec/models/voucher_request_spec.rb
+++ spec/models/voucher_request_spec.rb
@@ -14,10 +14,10 @@ RSpec.describe VoucherRequest, type: :model do
 
   describe "voucher input request" do
     it "should read a voucher request from disk" do
-      token = Base64.decode64(IO::read("spec/files/vr_JADA123456789.pkcs"))
+      token = Base64.decode64(IO::read("spec/files/parboiled_vr-00-D0-E5-F2-00-02.pkcs"))
       vr2 = VoucherRequest.from_pkcs7(token)
-      expect(vr2.device_identifier).to eq("JADA123456789")
-      expect(vr2.nonce).to eq("abcd1234")
+      expect(vr2.device_identifier).to eq("00-D0-E5-F2-00-02")
+      expect(vr2.nonce).to eq("Dss99sBr3pNMOACe-LYY7w")
       expect(vr2.owner).to_not be_nil
 
       voucher = vr2.issue_voucher('2017-09-15'.to_date)
@@ -27,10 +27,6 @@ RSpec.describe VoucherRequest, type: :model do
       expect(voucher.as_issued).to_not be_nil
 
       # save it for examination elsewhere (and use by Registrar tests)
-      File.open(File.join("tmp", "voucher_#{voucher.device_identifier}.pkcs"), "w") do |f|
-        f.puts voucher.as_issued
-      end
-
       expect(Chariwt.cmp_pkcs_file(voucher.as_issued, "voucher_#{voucher.device_identifier}")).to be true
 
       expect(voucher.owner.pubkey).to eq(vr2.signing_key)
