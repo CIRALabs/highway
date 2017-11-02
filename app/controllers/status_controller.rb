d7e5986776945866a95e47ad9b51e544871794d3
Michael Richardson <mcr@sandelman.ca>
2017-11-02 14:01:47 -0400

    add json version of status response

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,11 +1,22 @@
 class StatusController < ActionController::Base
 
   def index
-    @stats = [['Devices', Device.count],
-              ['Owners',  Owner.count],
-              ['Vouchers',Voucher.count],
-              ['Requests',VoucherRequest.count],
-             ]
-    render layout: 'reload'
+    respond_to do |format|
+      format.html {
+        @stats = [['Devices', Device.count],
+                  ['Owners',  Owner.count],
+                  ['Vouchers',Voucher.count],
+                  ['Requests',VoucherRequest.count],
+                 ]
+        render layout: 'reload'
+      }
+      format.json {
+        json_response({ 'Devices' => Device.count,
+                        'Owners'  => Owner.count,
+                        'Vouchers'=> Voucher.count,
+                        'Requests'=> VoucherRequest.count},
+                      :ok, 'application/json')
+      }
+    end
   end
 end
