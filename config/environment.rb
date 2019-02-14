a6960fd26316fd61cd3d0504f314d524da57eea3
Michael Richardson <mcr@sandelman.ca>
2019-02-14 13:20:54 -0500

    bump to 0.94 and set tag

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 $MASAUrl = 'https://highway.sandelman.ca/'
 $ADMINTERFACE = false
 
-$VERSION = "0.93"
+$VERSION = "0.94"
 
 # gets overritten by config/initializers/revision.rb by capistrano
 $REVISION= "devel"
