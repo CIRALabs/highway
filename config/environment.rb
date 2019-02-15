9f1ae087ce2a9494b943129335236c5a4ed7b1a6
Michael Richardson <mcr@sandelman.ca>
2019-02-15 18:56:16 -0500

    bump version

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 $MASAUrl = 'https://highway.sandelman.ca/'
 $ADMINTERFACE = false
 
-$VERSION = "0.94"
+$VERSION = "0.9.5"
 
 # gets overritten by config/initializers/revision.rb by capistrano
 $REVISION= "devel"
