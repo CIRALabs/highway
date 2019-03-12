cf53d26c394dab75558233b26b14a4d080890cb3
Michael Richardson <mcr@sandelman.ca>
2019-03-12 18:26:17 -0400

    added shg-provision URL

--- spec/routing/est_routing_spec.rb
+++ spec/routing/est_routing_spec.rb
@@ -10,5 +10,10 @@ RSpec.describe EstController, type: :routing do
     it "routes to #smarkaklink" do
       expect(:post => "/smarkaklink/enroll").to route_to("smarkaklink#enroll")
     end
+
+    it "routes to #shgprovision" do
+      expect(:post => "/shg-provision").to route_to("smarkaklink#provision")
+    end
+
   end
 end
