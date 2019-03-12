3fcccdb5e87f055a704ddda89f2813ab43bbc8ff
Michael Richardson <mcr@sandelman.ca>
2019-03-12 19:29:24 -0400

    added generic attributes column in json, and specific secondary eui64 so that wifi/lan/wan address can be stored

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_030939) do
+ActiveRecord::Schema.define(version: 2019_03_12_230246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2019_02_25_030939) do
     t.text "fqdn"
     t.text "essid"
     t.text "ula"
+    t.json "attributes"
+    t.text "second_eui64"
   end
 
   create_table "owners", force: :cascade do |t|
