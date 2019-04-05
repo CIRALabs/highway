a9a1b0e31f07f8f56e33ed5d7ddaeae3494812c5
Michael Richardson <mcr@sandelman.ca>
2019-04-05 17:53:59 -0400

    updated migration to fix blank

--- db/migrate/20190405212807_translate_idevid_cert_to_pub_key.rb
+++ db/migrate/20190405212807_translate_idevid_cert_to_pub_key.rb
@@ -1,7 +1,7 @@
 class TranslateIDevIDCertToPubKey < ActiveRecord::Migration[5.2]
   def change
     Device.all.each {|a|
-      if a.idevid_cert.blank? and !a.pub_key.blank and a.pub_key.include?("BEGIN CERTIFICATE")
+      if a.idevid_cert.blank? and !a.pub_key.blank? and a.pub_key.include?("BEGIN CERTIFICATE")
         a.idevid_cert = a.pub_key
         a.set_public_key(a.certificate.public_key)
       end
