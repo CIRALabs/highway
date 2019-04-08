1651b1eb6fcd65dae9df7cfb08f039b638d7b2b0
Michael Richardson <mcr@sandelman.ca>
2019-04-08 19:42:29 -0400

    rename migration to run again, save data after running it

--- db/migrate/20190405212809_translate_idevid_cert_to_pub_key.rb
+++ db/migrate/20190405212809_translate_idevid_cert_to_pub_key.rb
@@ -1,9 +1,11 @@
 class TranslateIDevIDCertToPubKey < ActiveRecord::Migration[5.2]
-  def change
+  def up
     Device.all.each {|a|
       if a.idevid_cert.blank? and !a.pub_key.blank? and a.pub_key.include?("BEGIN CERTIFICATE")
+        puts "  ..converting #{a.id}"
         a.idevid_cert = a.pub_key
         a.set_public_key(a.certificate.public_key)
+        a.save!
       end
     }
   end
