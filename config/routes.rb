65f6f01503958e10f3164bd0870548f1297d95a9
Michael Richardson <mcr@sandelman.ca>
2019-06-10 14:32:19 -0400

    added robots.txt and security.txt

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: static("index.html")
   get '/favicon.ico', to: static("favicon.ico")
+  get '/robots.txt',  to: static("robots.txt")
+  get '/.well-known/security.txt',  to: static("security.txt")
 
   if $ADMININTERFACE or Rails.env == 'test'
   devise_for :admins
