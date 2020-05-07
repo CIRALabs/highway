03e3fc2b453f6c7ddc7cb53584199da6d2cd9c75
Michael Richardson <mcr@sandelman.ca>
2020-05-07 15:40:16 -0400

    added deploy instructions for MASA store wheezes

---
+++ config/deploy/wheezes.rb
@@ -0,0 +1,47 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
+
+# connect the hook directly
+after :'deploy:log_revision', :'passenger:restart'
+
+  set :deploy_to, '/wheezes/app/highway'
+
+  server "masa.wheezes.sandelman.ca",
+       user: "wheezes",
+       roles: %w{app db web},
+       deploy_to: '/wheezes/app/highway',
+       git_wrapper_path: '/wheezes/app/tmp'
+
+set :stage, :production
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+set :ssh_options, {
+      forward_agent: true,
+    }
+
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
