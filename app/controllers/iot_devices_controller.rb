3246f23164f685a672a7e7dd60e0ef02d0f0407a
Michael Richardson <mcr@sandelman.ca>
2020-08-16 23:50:55 -0400

    do authorization tests for send_new_device_notification

--- app/controllers/iot_devices_controller.rb
+++ app/controllers/iot_devices_controller.rb
@@ -2,15 +2,20 @@ class IotDevicesController < ApiController
 
   # /send_new_device_notification
   def new
-    @clientcert = capture_client_certificate
+    @clientpem = capture_client_certificate
 
-    unless @clientcert
+    unless @clientpem
       head 403, text: "invalid authorization, no certificate provided"
       return
     end
 
     # now locate the (gateway) device based upon the certificate provided.
-    @device = Device.get_router_by_identity(@clientcert)
+    @device = Device.get_router_by_identity(@clientpem)
+    unless @device
+      head 404, text: "authorization device is unknown"
+    end
+
+    head 200, text: "new device notification sent"
   end
 
   def analysis_complete
--- spec/requests/iot_devices_request_spec.rb
+++ spec/requests/iot_devices_request_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "IotDevices", type: :request do
+  fixtures :all
 
   describe "sshg notification" do
     it "should fail when no certificate has been provided to /send_new_device_notification" do
@@ -8,6 +9,29 @@ RSpec.describe "IotDevices", type: :request do
       post "/send_new_device_notification"
 
       expect(response).to have_http_status(403)
+    end
+
+    it "should fail when the certificate does not belong to any valid device" do
+      # /send_new_device_notification maps to IotDevicesController#new
+      # use a certificate from the borgin (hostile) MASA
+      pubkey_pem = File.read("spec/files/borgin/00-D0-E5-F3-00-02/device.crt")
+      token = {}
+      post "/send_new_device_notification", params: token, headers: {
+             'SSL_CLIENT_CERT'=> pubkey_pem
+           }
+
+      expect(response).to have_http_status(404)
+    end
+
+    it "POST /send_new_device_notification" do
+      # /send_new_device_notification maps to IotDevicesController#new
+      # use a certificate from our internal test devices
+      pubkey_pem = devices(:zeb).certificate.to_pem
+      token = {}
+      post "/send_new_device_notification", params: token, headers: {
+             'SSL_CLIENT_CERT'=> pubkey_pem
+           }
+      expect(response).to have_http_status(200)
     end
 
     it "POST /send_done_analyzing_notification" do
