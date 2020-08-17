d1f60ad08caecb8add326ab04d7f7434fcb31ca4
Michael Richardson <mcr@sandelman.ca>
2020-08-16 21:46:51 -0400

    added test case and code for case where no authentication is provided

--- app/controllers/iot_devices_controller.rb
+++ app/controllers/iot_devices_controller.rb
@@ -1,6 +1,16 @@
 class IotDevicesController < ApiController
 
+  # /send_new_device_notification
   def new
+    @clientcert = capture_client_certificate
+
+    unless @clientcert
+      head 403, text: "invalid authorization, no certificate provided"
+      return
+    end
+
+    # now locate the (gateway) device based upon the certificate provided.
+    @device = Device.get_router_by_identity(@clientcert)
   end
 
   def analysis_complete
--- spec/requests/iot_devices_request_spec.rb
+++ spec/requests/iot_devices_request_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe "IotDevices", type: :request do
 
   describe "sshg notification" do
-    it "POST /send_new_device_notification" do
+    it "should fail when no certificate has been provided to /send_new_device_notification" do
+      # /send_new_device_notification maps to IotDevicesController#new
       post "/send_new_device_notification"
 
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(403)
     end
 
     it "POST /send_done_analyzing_notification" do
